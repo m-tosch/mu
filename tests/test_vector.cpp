62cd9691a19ffdd89c6db4f3c26145e4c692f449
m-tosch <m-tosch@users.noreply.github.com>
2020-06-11 21:54:53 +0200

    constructor value init unit test

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,7 +1,17 @@
 #include "gtest/gtest.h"
 #include "vector.h"
 
-TEST(sample_test_case, sample_test) {
+TEST(Vector, sample_test) {
   // Vector<2, float> v;
+  // int a = 5 + 5;
   EXPECT_EQ(1, 1);
+}
+
+TEST(Vector, ConstructorSingleValueInit) {
+  // arrange
+  Vector<2, float> v{0.0f};
+
+  // assert
+  EXPECT_FLOAT_EQ(v[0], 0.0f);
+  EXPECT_FLOAT_EQ(v[1], 0.0f);
 }
