28ef57a985815fde9338ddbd13d8d59579d1caf3
m-tosch <m-tosch@users.noreply.github.com>
2020-06-13 17:26:08 +0200

    remove junk

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,17 +1,11 @@
 #include "gtest/gtest.h"
 #include "vector.h"
 
-TEST(Vector, sample_test) {
-  // Vector<2, float> v;
-  // int a = 5 + 5;
-  EXPECT_EQ(1, 1);
-}
-
 TEST(Vector, ConstructorSingleValueInit) {
   // arrange
-  Vector<2, float> v{0.0f};
+  Vector<2, float> v{0.0F};
 
   // assert
-  EXPECT_FLOAT_EQ(v[0], 0.0f);
-  EXPECT_FLOAT_EQ(v[1], 0.0f);
+  EXPECT_FLOAT_EQ(v[0], 0.0F);
+  EXPECT_FLOAT_EQ(v[1], 0.0F);
 }
