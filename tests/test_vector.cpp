f33df91f62dc732c86d8453d8b7fdb551c80b7ab
m-tosch <m-tosch@users.noreply.github.com>
2020-06-16 20:47:34 +0200

    move code down

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -8,17 +8,17 @@ TEST(Vector, ConstructorDefault) {
   EXPECT_TRUE(c);
 }
 
-TEST(Vector, DestructDefault) {
-  // arrange
-  bool d = std::is_destructible<Vector<2, int>>::value;
-  // assert
-  EXPECT_TRUE(d);
-}
-
 TEST(Vector, ConstructorSingleValueInit) {
   // arrange
   Vector<2, float> v{0.0F};
   // assert
   EXPECT_FLOAT_EQ(v[0], 0.0F);
   EXPECT_FLOAT_EQ(v[1], 0.0F);
+}
+
+TEST(Vector, DestructDefault) {
+  // arrange
+  bool d = std::is_destructible<Vector<2, int>>::value;
+  // assert
+  EXPECT_TRUE(d);
 }
