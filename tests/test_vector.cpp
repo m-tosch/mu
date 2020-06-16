509a42fdce72b3abb42f642908319668cc0c355f
m-tosch <m-tosch@users.noreply.github.com>
2020-06-16 20:57:37 +0200

    vector move assignment operator unit test

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -49,3 +49,12 @@ TEST(Vector, OperatorCopyAssignment) {
   /** assert */
   EXPECT_FLOAT_EQ(v2[0], 2.0F);
 }
+
+TEST(Vector, OperatorMoveAssignment) {
+  /** arrange */
+  Vector<1, float> v1{2.0F};
+  /** action */
+  Vector<1, float> v2 = std::move(v1);
+  /** assert */
+  EXPECT_FLOAT_EQ(v2[0], 2.0F);
+}
