d9c8e39cf7093a91dfab755174c200fd3905652d
m-tosch <m-tosch@users.noreply.github.com>
2020-06-16 20:54:26 +0200

    doxygen comment style

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -2,32 +2,32 @@
 #include "vector.h"
 
 TEST(Vector, ConstructorDefault) {
-  // arrange
+  /** arrange */
   bool c = std::is_default_constructible<Vector<1, int>>::value;
-  // assert
+  /** assert */
   EXPECT_TRUE(c);
 }
 
 TEST(Vector, ConstructorSingleValueInit) {
-  // arrange
+  /** arrange */
   Vector<2, float> v{0.0F};
-  // assert
+  /** assert */
   EXPECT_FLOAT_EQ(v[0], 0.0F);
   EXPECT_FLOAT_EQ(v[1], 0.0F);
 }
 
 TEST(Vector, DestructorDefault) {
-  // arrange
+  /** arrange */
   bool d = std::is_destructible<Vector<1, int>>::value;
-  // assert
+  /** assert */
   EXPECT_TRUE(d);
 }
 
 TEST(Vector, ConstructorCopy) {
-  // arrange
+  /** arrange */
   Vector<1, float> v1{2.0F};
-  // action
+  /** action */
   Vector<1, float> v2{v1};
-  // assert
+  /** assert */
   EXPECT_FLOAT_EQ(v2[0], 2.0F);
 }
