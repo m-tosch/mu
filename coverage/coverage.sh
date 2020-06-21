8d77b27bdbb2af0cce0d9651103c3003ebc7b061
m-tosch <m-tosch@users.noreply.github.com>
2020-06-21 17:57:18 +0200

    move coverage script

--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -2,9 +2,9 @@
 # 01. build with cmake
 # 02. google test main exectuable (generate .gcda files)
 
-WORKDIR="build/tests/CMakeFiles/mu_tests.dir"
+WORKDIR="../build/tests/CMakeFiles/mu_tests.dir"
 # 1. generate coverage .info (lcov uses gcov)
 # exclude all external headers then include only the ones for this project
-lcov --directory $WORKDIR --capture --output-file $WORKDIR/main_coverage.info --no-external --directory ./include
+lcov --directory $WORKDIR --capture --output-file $WORKDIR/main_coverage.info --no-external --directory ../include
 # 2. generate html report
-genhtml $WORKDIR/main_coverage.info --output-directory coverage
+genhtml $WORKDIR/main_coverage.info --output-directory html
