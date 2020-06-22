83b2f532bd49f9f5c3d22eb08465bf8ae7ae7502
m-tosch <m-tosch@users.noreply.github.com>
2020-06-22 12:19:15 +0200

    comments

--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -1,11 +1,12 @@
 # project root directory relative to the location of this file
 ROOTDIR=".."
+# working directory for intermediate coverage files
 WORKDIR="$ROOTDIR/build/tests/CMakeFiles/mu_tests.dir"
 
-# 1. build with cmake
+# 1. cmake build
 # note: same command as in .vscode/tasks.json
 mkdir -p $ROOTDIR/build && cd $ROOTDIR/build && cmake .. -G \"Unix Makefiles\" && make
-# 2. google test main exectuable (generate .gcda files)
+# 2. google test main exectuable. only print test failures (generate .gcda files)
 # note: same command as in .vscode/tasks.json
 $ROOTDIR/build/tests/mu_tests --gtest_brief=1
 # 3. generate coverage .info (lcov uses gcov)
