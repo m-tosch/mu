d60d789383380ed7bd8c346e69bdcebc32496b52
m-tosch <m-tosch@users.noreply.github.com>
2020-06-29 19:23:22 +0200

    naive vector equality operator

--- include/mu/vector.h
+++ include/mu/vector.h
@@ -69,6 +69,27 @@ class Vector {
   Vector& operator=(Vector&& other) noexcept = default;
 
   /**
+   * @brief compare two objects for equality
+   *
+   * @tparam Nn size
+   * @tparam Tt type
+   * @param other
+   * @return bool
+   */
+  template <std::size_t Nn, typename Tt>
+  bool operator==(const Vector<Nn, Tt>& other) const {
+    if (N != Nn) {
+      return false;
+    }
+    for (std::size_t i = 0; i < N; i++) {
+      if (data_[i] != other[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
    * @brief access an element within the vector
    *
    * does not throw an exception if \p idx is out of range
