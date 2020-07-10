391137dc27661d27146abce06157704aead289ab
m-tosch <m-tosch@users.noreply.github.com>
2020-07-10 16:29:57 +0200

    initial Vector2D

---
+++ include/mu/vector2d.h
@@ -0,0 +1,14 @@
+#ifndef MU_VECTOR2D_H_
+#define MU_VECTOR2D_H_
+
+#include <vector.h>
+
+namespace mu {
+
+template <typename T>
+class Vector2D : Vector<2, T> {
+ public:
+};
+
+}  // namespace mu
+#endif  // MU_VECTOR2D_H_
