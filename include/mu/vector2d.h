8ab40a972bc82a01ecfc04aac64931a562c2891b
m-tosch <m-tosch@users.noreply.github.com>
2020-07-10 16:30:13 +0200

    init Vector2D x,y

--- include/mu/vector2d.h
+++ include/mu/vector2d.h
@@ -8,6 +8,10 @@ namespace mu {
 template <typename T>
 class Vector2D : Vector<2, T> {
  public:
+  Vector2D(T x, T y) {
+    this->data_[0] = x;
+    this->data_[1] = y;
+  }
 };
 
 }  // namespace mu
