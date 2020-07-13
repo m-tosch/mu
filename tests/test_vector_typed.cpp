0d15dc01b710de198142d5f0b5b6a90e90ec9e94
m-tosch <m-tosch@users.noreply.github.com>
2020-07-13 20:15:40 +0200

    initial typed vector test with tuples

--- tests/test_vector_typed.cpp
+++ tests/test_vector_typed.cpp
@@ -1,32 +1,33 @@
+#include <tuple>
+#include <utility>
+
 #include "gtest/gtest.h"
 #include "vector.h"
 
 template <typename T>
 class VectorFixture : public ::testing::Test {
  public:
-  virtual void SetUp() {
-    value_[0] = 1.0F;
-    value_[1] = 2.0F;
-  }
-  typedef mu::Vector<2, T> VecType;
-  VecType value_;
+  virtual void SetUp() {}
+  typedef typename std::tuple_element<0, T>::type T1;
+  typedef typename std::tuple_element<1, T>::type T2;
 };
 
-using VectorTypes = ::testing::Types<char, int, float>;
+using VectorTypes =
+    ::testing::Types<std::tuple<mu::Vector<2, float>, mu::Vector<2, float>>>;
 TYPED_TEST_SUITE(VectorFixture, VectorTypes);
 
 TYPED_TEST(VectorFixture, ConstructorDefault) {
-  typename VectorFixture::VecType vvv;
+  typedef typename TestFixture::T1 Ftype;
   /** arrange */
-  bool c = std::is_default_constructible<vvv>::value;
+  bool c = std::is_default_constructible<Ftype>::value;
   /** assert */
   EXPECT_TRUE(c);
 }
 
-TYPED_TEST(VectorFixture, ConstructorSingleValueInit) {
-  /** arrange */
-  mu::Vector<2, float> v{2.0F};
-  /** assert */
-  EXPECT_FLOAT_EQ(v[0], 2.0F);
-  EXPECT_FLOAT_EQ(v[1], 2.0F);
-}
+// TYPED_TEST(VectorFixture, ConstructorSingleValueInit) {
+//   /** arrange */
+//   mu::Vector<2, float> v{2.0F};
+//   /** assert */
+//   EXPECT_FLOAT_EQ(v[0], 2.0F);
+//   EXPECT_FLOAT_EQ(v[1], 2.0F);
+// }
