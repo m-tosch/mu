3abfb46ec4c053a40236dec618a448c7a2ecaf49
m-tosch <m-tosch@users.noreply.github.com>
2020-07-17 21:12:31 +0200

    delete first few old tests

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -8,98 +8,13 @@
  */
 template class mu::Vector<2, float>;
 
+/**
+ * Instantiate the Vector test suite
+ */
 using VectorTypes = ::testing::Types<mu::Vector<2, float>, mu::Vector<2, int>>;
 INSTANTIATE_TYPED_TEST_SUITE_P(Vector, VectorTypeFixture, VectorTypes);
 
 /******************************************************************************/
-
-TEST(Vector, ConstructorDefault) {
-  /** arrange */
-  bool c = std::is_default_constructible<mu::Vector<1, float>>::value;
-  /** assert */
-  EXPECT_TRUE(c);
-}
-
-TEST(Vector, ConstructorSingleValueInit) {
-  /** arrange */
-  mu::Vector<2, float> v{2.0F};
-  /** assert */
-  EXPECT_FLOAT_EQ(v[0], 2.0F);
-  EXPECT_FLOAT_EQ(v[1], 2.0F);
-}
-
-TEST(Vector, DestructorDefault) {
-  /** arrange */
-  bool d = std::is_destructible<mu::Vector<1, int>>::value;
-  /** assert */
-  EXPECT_TRUE(d);
-}
-
-TEST(Vector, ConstructorCopy) {
-  /** arrange */
-  mu::Vector<1, float> v1{2.0F};
-  /** action */
-  mu::Vector<1, float> v2{v1};
-  /** assert */
-  EXPECT_FLOAT_EQ(v2[0], 2.0F);
-}
-
-TEST(Vector, ConstructorMove) {
-  /** arrange */
-  mu::Vector<1, float> v1{2.0F};
-  /** action */
-  mu::Vector<1, float> v2{std::move(v1)};
-  /** assert */
-  EXPECT_FLOAT_EQ(v2[0], 2.0F);
-}
-
-TEST(Vector, OperatorCopyAssignment) {
-  /** arrange */
-  mu::Vector<1, float> v1{2.0F};
-  /** action */
-  mu::Vector<1, float> v2 = v1;
-  /** assert */
-  EXPECT_FLOAT_EQ(v2[0], 2.0F);
-}
-
-TEST(Vector, OperatorMoveAssignment) {
-  /** arrange */
-  mu::Vector<1, float> v1{2.0F};
-  /** action */
-  mu::Vector<1, float> v2 = std::move(v1);
-  /** assert */
-  EXPECT_FLOAT_EQ(v2[0], 2.0F);
-}
-
-TEST(Vector, OperatorEqualsValuesSame) {
-  /** arrange */
-  mu::Vector<2, float> v1{1.0F};
-  mu::Vector<2, float> v2{1.0F};
-  /** action */
-  bool eq = (v1 == v2);
-  /** assert */
-  EXPECT_TRUE(eq);
-}
-
-TEST(Vector, OperatorEqualsValuesDifferent) {
-  /** arrange */
-  mu::Vector<2, float> v1{1.0F};
-  mu::Vector<2, float> v2{2.0F};
-  /** action */
-  bool eq = (v1 == v2);
-  /** assert */
-  EXPECT_FALSE(eq);
-}
-
-TEST(Vector, OperatorEqualsLengthDifferent) {
-  /** arrange */
-  mu::Vector<2, float> v1{1.0F};
-  mu::Vector<3, float> v2{1.0F};
-  /** action */
-  bool eq = (v1 == v2);
-  /** assert */
-  EXPECT_FALSE(eq);
-}
 
 TEST(Vector, OperatorPlus) {
   /** arrange */
@@ -121,28 +36,6 @@ TEST(Vector, OperatorMinus) {
   /** assert */
   EXPECT_FLOAT_EQ(res[0], v1[0] - v2[1]);
   EXPECT_FLOAT_EQ(res[1], v1[0] - v2[1]);
-}
-
-TEST(Vector, OperatorBrackets) {
-  /** arrange */
-  mu::Vector<2, float> v{2.0F};
-  /** action */
-  float a = v[0];
-  float b = v[1];
-  /** assert */
-  EXPECT_FLOAT_EQ(a, 2.0F);
-  EXPECT_FLOAT_EQ(b, 2.0F);
-}
-
-TEST(Vector, OperatorConstBrackets) {
-  /** arrange */
-  const mu::Vector<2, float> v{2.0F};
-  /** action */
-  float a = v[0];
-  float b = v[1];
-  /** assert */
-  EXPECT_FLOAT_EQ(a, 2.0F);
-  EXPECT_FLOAT_EQ(b, 2.0F);
 }
 
 TEST(Vector, AccessAtFunction) {
