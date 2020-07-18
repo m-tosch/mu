b33e0f7f8c85f2b7fbc8f6cb21c5e5b4a283d712
m-tosch <m-tosch@users.noreply.github.com>
2020-07-18 12:13:26 +0200

    first vector2d specific tests

--- tests/test_vector_2d.cpp
+++ tests/test_vector_2d.cpp
@@ -1,3 +1,5 @@
+#include <gtest/gtest-typed-test.h>
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "test_vector_type.h"
 #include "vector2d.h"
@@ -15,3 +17,32 @@ using Vector2DTypes = ::testing::Types<mu::Vector2D<float>, mu::Vector2D<int>>;
 INSTANTIATE_TYPED_TEST_SUITE_P(Vector2D, VectorTypeFixture, Vector2DTypes);
 
 /******************************************************************************/
+
+template <typename T>
+class Vector2DTypeFixture : public VectorTypeFixture<T> {
+ public:
+  void SetUp() override {
+    /* explicitly call base class setup */
+    VectorTypeFixture<T>::SetUp();
+  }
+};
+
+TYPED_TEST_SUITE(Vector2DTypeFixture, Vector2DTypes);
+
+TYPED_TEST(Vector2DTypeFixture, MemberFuncGetX) {
+  /** arrange */
+  TypeParam obj{this->values};
+  /** action */
+  typename TypeParam::value_type x = obj.x();
+  /** assert */
+  EXPECT_EQ(x, this->values[0]);
+}
+
+TYPED_TEST(Vector2DTypeFixture, MemberFuncGetY) {
+  /** arrange */
+  TypeParam obj{this->values};
+  /** action */
+  typename TypeParam::value_type y = obj.y();
+  /** assert */
+  EXPECT_EQ(y, this->values[1]);
+}
