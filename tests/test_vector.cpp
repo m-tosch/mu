43f15e6f49ee33e6dc6121cda0cc1c24ce37b774
m-tosch <m-tosch@users.noreply.github.com>
2020-08-02 10:33:35 +0200

    initial vector combination tests

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -13,27 +13,3 @@ template class mu::Vector<2, float>;
  */
 using VectorTypes = ::testing::Types<mu::Vector<2, float>, mu::Vector<2, int>>;
 INSTANTIATE_TYPED_TEST_SUITE_P(Vector, VectorTypeFixture, VectorTypes);
-
-/******************************************************************************/
-
-TEST(Vector, OperatorPlus) {
-  /** arrange */
-  mu::Vector<2, float> v1{1.0F};
-  mu::Vector<2, float> v2{1.0F};
-  /** action */
-  mu::Vector<2, float> res = v1 + v2;
-  /** assert */
-  EXPECT_FLOAT_EQ(res[0], v1[0] + v2[1]);
-  EXPECT_FLOAT_EQ(res[1], v1[0] + v2[1]);
-}
-
-TEST(Vector, OperatorMinus) {
-  /** arrange */
-  mu::Vector<2, float> v1{1.0F};
-  mu::Vector<2, float> v2{1.0F};
-  /** action */
-  mu::Vector<2, float> res = v1 - v2;
-  /** assert */
-  EXPECT_FLOAT_EQ(res[0], v1[0] - v2[1]);
-  EXPECT_FLOAT_EQ(res[1], v1[0] - v2[1]);
-}
--- tests/test_vector_combinations.cpp
+++ tests/test_vector_combinations.cpp
@@ -1,13 +1,52 @@
-/*
 
-Vector - Vector
-Vector - Vector2D
-Vector2D - Vector
-Vector2D - Vector
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
-addition
-subtraction
-equals
-...
+#include <algorithm>
+#include <tuple>
 
-*/
+#include "test_vector_type.h"
+#include "vector.h"
+#include "vector2d.h"
+
+using VectorTypeCombinations =
+    ::testing::Types<std::tuple<mu::Vector<2, float>, mu::Vector<2, float>>>;
+
+template <typename T>
+class VectorCombinationsFixture
+    : public VectorTypeFixture<typename std::tuple_element<0, T>::type> {
+ public:
+  typedef typename std::tuple_element<0, T>::type T1;
+  typedef typename std::tuple_element<1, T>::type T2;
+};
+
+TYPED_TEST_SUITE(VectorCombinationsFixture, VectorTypeCombinations);
+
+TYPED_TEST(VectorCombinationsFixture, OperatorAddition) {
+  /** arrange */
+  typename TestFixture::T1 obj1{this->values};
+  typename TestFixture::T2 obj2{this->values};
+  /** action */
+  typename TestFixture::T1 res = obj1 + obj2;
+  /** assert */
+  typename TestFixture::T1 comp;
+  std::generate(comp.begin(), comp.end(), [&obj1, &obj2, i = 0]() mutable {
+    return obj1[i] + obj2[i++];
+  });
+  EXPECT_THAT(res, ::testing::ContainerEq(comp));
+}
+
+TYPED_TEST(VectorCombinationsFixture, OperatorSubtraction) {
+  /** arrange */
+  typename TestFixture::T1 obj1{this->values};
+  typename TestFixture::T2 obj2{this->values};
+  /** action */
+  typename TestFixture::T1 res = obj1 - obj2;
+  /** assert */
+  typename TestFixture::T1 comp;
+  std::generate(comp.begin(), comp.end(), [&obj1, &obj2, i = 0]() mutable {
+    return obj1[i] - obj2[i++];
+  });
+  EXPECT_THAT(res, ::testing::ContainerEq(comp));
+}
