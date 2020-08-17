e7adc045b9aba9f1e3d86c3429635d4b112f881c
m-tosch <m-tosch@users.noreply.github.com>
2020-08-17 20:21:10 +0200

    vector2d use functions instead of members

--- include/mu/vector2d.h
+++ include/mu/vector2d.h
@@ -17,15 +17,13 @@ class Vector2D : public Vector<2, T> {
    * defined here. apply rule of zero */
   using Vector<2, T>::Vector;
 
-  // constexpr Vector2D() : x(0), y(0) {}
-
   /**
    * @brief Construct a new Vector2D object from two values
    *
    * @param x
    * @param y
    */
-  Vector2D(T x, T y) : x(x), y(y) {
+  Vector2D(T x, T y) {
     this->data_[0] = x;
     this->data_[1] = y;
   }
@@ -41,8 +39,10 @@ class Vector2D : public Vector<2, T> {
   // NOLINTNEXTLINE(runtime/explicit) implicit conversion is intentional
   Vector2D(const Vector<2, T>& other) : Vector<2, T>(other) {}
 
-  T x; /**< x value */
-  T y; /**< y value */
+  T& x() noexcept { return this->data_[0]; }
+  const T& x() const noexcept { return this->data_[0]; }
+  T& y() noexcept { return this->data_[1]; }
+  const T& y() const noexcept { return this->data_[1]; }
 };
 
 }  // namespace mu
