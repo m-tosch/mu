7f6f429737274932fac1b3db4a559d953c167bbf
m-tosch <m-tosch@users.noreply.github.com>
2020-09-06 17:27:07 +0200

    instantiate length()

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -3,11 +3,13 @@
 #include "mu/vector.h"
 #include "vector_type.h"
 
+
 /**
  * Instantiate this template class and template functions explicitly so that all
  * functions are generated and thus, the coverage report is accurate.
  */
 template class mu::Vector<2, float>;
+template float mu::Vector<2, float>::length<float>() const;
 
 /**
  * Instantiate the Vector test suite
