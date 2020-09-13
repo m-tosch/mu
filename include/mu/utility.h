4c945ee50f9055ff3a79c449a348d7e0a460d133
m-tosch <m-tosch@users.noreply.github.com>
2020-09-13 17:36:57 +0200

    sin()

--- include/mu/utility.h
+++ include/mu/utility.h
@@ -1,6 +1,17 @@
 #ifndef MU_UTILITY_H_
 #define MU_UTILITY_H_
 
-namespace mu {}
+#include <cmath>
+#include <type_traits>
+
+namespace mu {
+
+template <typename T>
+typename std::enable_if<std::is_floating_point<T>::value, T>::type sin(
+    T value) {
+  return std::sin(value);
+}
+
+}  // namespace mu
 
 #endif  // MU_UTILITY_H_
