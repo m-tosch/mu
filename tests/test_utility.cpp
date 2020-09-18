02bb6a583ffd317f4b417ae71608b03737792a08
m-tosch <m-tosch@users.noreply.github.com>
2020-09-18 19:58:45 +0200

    prepare value param test

--- tests/test_utility.cpp
+++ tests/test_utility.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 
+#include "mu/literals.h"
 #include "mu/utility.h"
 
 /**
@@ -18,10 +19,12 @@ using FloatingPointTypes = ::testing::Types<float, double, long double>;
 
 /********************************Utility***************************************/
 
-template <typename T>
-class UtilityFixture : public ::testing::Test {};
+// template <typename T>
+class UtilityFixture : public ::testing::TestWithParam<float> {};
 
-TYPED_TEST_SUITE(UtilityFixture, FloatingPointTypes);
+INSTANTIATE_TEST_SUITE_P(InstantiationName,  // just the name
+                         UtilityFixture,
+                         ::testing::Values(mu::kPI, mu::kPI2, mu::kPI4));
 
 /* TODO value parameterized tests for mu::sin() */
 
