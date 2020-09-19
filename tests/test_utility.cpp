72d29f89b2f128d93955bff9d8296b5492875bc0
m-tosch <m-tosch@users.noreply.github.com>
2020-09-19 17:43:15 +0200

    test fixture

--- tests/test_utility.cpp
+++ tests/test_utility.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 
 #include <cmath>
+#include <vector>
 
 #include "mu/literals.h"
 #include "mu/utility.h"
+#include "typetraits.h"
 
 /**
  * Instantiate the template functions explicitly so that all
@@ -12,20 +14,19 @@
 template float mu::sin<float>(float);
 template float mu::cos<float>(float);
 
-/**
- * Instantiate the Vector test suite
+/*
+ * floating point types
  */
 using FloatingPointTypes = ::testing::Types<float, double, long double>;
 
 /********************************Utility***************************************/
 
-// template <typename T>
-class UtilityFixture : public ::testing::TestWithParam<float> {};
+template <typename T>
+class UtilityFixture : public ::testing::Test {
+ public:
+  /* [-2pi | -pi | -pi/2 | -pi/4 | 0 | pi/4 | pi/2 | pi | 2pi] */
+  std::vector<T> values = {-2 * mu::kPI, -mu::kPI, -mu::kPI2, -mu::kPI4,  0,
+                           mu::kPI4,     mu::kPI2, mu::kPI,   2 * mu::kPI};
+};
 
-INSTANTIATE_TEST_SUITE_P(InstantiationName,  // just the name
-                         UtilityFixture,
-                         ::testing::Values(mu::kPI, mu::kPI2, mu::kPI4));
-
-/* TODO value parameterized tests for mu::sin() */
-
-/* TODO value parameterized tests for mu::cos() */
+TYPED_TEST_SUITE(UtilityFixture, FloatingPointTypes);
