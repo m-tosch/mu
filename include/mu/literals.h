488bfee846b02f36350a635ff330984de95e91de
m-tosch <m-tosch@users.noreply.github.com>
2020-09-22 16:41:15 +0200

    ln literals

--- include/mu/literals.h
+++ include/mu/literals.h
@@ -8,6 +8,8 @@ namespace mu {
 constexpr double e = M_E;               /* euler constant */
 constexpr double log2e = M_LOG2E;       /* log2(e) */
 constexpr double log10e = M_LOG10E;     /* log10(e) */
+constexpr double ln2 = M_LN2;           /* ln(2) */
+constexpr double ln10 = M_LN10;         /* ln(10) */
 constexpr double pi = M_PI;             /* pi */
 constexpr double pi2 = M_PI_2;          /* pi/2 */
 constexpr double pi4 = M_PI_4;          /* pi/4 */
