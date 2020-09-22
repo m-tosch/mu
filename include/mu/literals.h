b268db797e3982a0146517135b1a7d8181d36cb7
m-tosch <m-tosch@users.noreply.github.com>
2020-09-22 16:39:15 +0200

    more literals

--- include/mu/literals.h
+++ include/mu/literals.h
@@ -5,9 +5,15 @@
 
 namespace mu {
 
-constexpr double PI = M_PI;
-constexpr double PI2 = M_PI_2;
-constexpr double PI4 = M_PI_4;
+constexpr double e = M_E;               /* euler constant */
+constexpr double log2e = M_LOG2E;       /* log2(e) */
+constexpr double log10e = M_LOG10E;     /* log10(e) */
+constexpr double pi = M_PI;             /* pi */
+constexpr double pi2 = M_PI_2;          /* pi/2 */
+constexpr double pi4 = M_PI_4;          /* pi/4 */
+constexpr double inv_pi = M_1_PI;       /* 1/pi */
+constexpr double sqrt2 = M_SQRT2;       /* sqrt(2) */
+constexpr double inv_sqrt2 = M_SQRT1_2; /* 1/sqrt(2) */
 
 }  // namespace mu
 
