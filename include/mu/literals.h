cd13a50d7961a5c0feda8a4c155c4331f9b99671
m-tosch <m-tosch@users.noreply.github.com>
2020-09-22 16:30:16 +0200

    use double

--- include/mu/literals.h
+++ include/mu/literals.h
@@ -5,9 +5,9 @@
 
 namespace mu {
 
-constexpr float PI = M_PI;
-constexpr float PI2 = M_PI_2;
-constexpr float PI4 = M_PI_4;
+constexpr double PI = M_PI;
+constexpr double PI2 = M_PI_2;
+constexpr double PI4 = M_PI_4;
 
 }  // namespace mu
 
