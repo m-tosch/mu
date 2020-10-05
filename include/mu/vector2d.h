b2728333fdc5328010d0496e7c65597eedba5dec
m-tosch <m-tosch@users.noreply.github.com>
2020-10-05 17:04:31 +0200

    use mu instead of std

--- include/mu/vector2d.h
+++ include/mu/vector2d.h
@@ -1,93 +1,95 @@
 #ifndef MU_VECTOR2D_H_
 #define MU_VECTOR2D_H_
 
+#include "mu/utility.h"
 #include "mu/vector.h"
 
 namespace mu {
 
-/**
- * @brief A two dimensional vector
- *
- * @tparam T type
- */
-template <typename T>
-class Vector2D : public Vector<2, T> {
- public:
-  /* inherit base class constructors. only constructors specific to Vector2D are
-   * defined here. apply rule of zero */
-  using Vector<2, T>::Vector;
-
   /**
-   * @brief Construct a new Vector2D object from a Vector object
+   * @brief A two dimensional vector
    *
-   * also implicitly used for copy initialization. purposely not marked
-   * "explicit" for exactly that usecase. suppress linting for this line.
-   *
-   * @param other
+   * @tparam T type
    */
-  // NOLINTNEXTLINE(runtime/explicit) implicit conversion is intentional
-  Vector2D(const Vector<2, T>& other) : Vector<2, T>(other) {}
+  template <typename T>
+  class Vector2D : public Vector<2, T> {
+   public:
+    /* inherit base class constructors. only constructors specific to Vector2D
+     * are defined here. apply rule of zero */
+    using Vector<2, T>::Vector;
 
-  /**
-   * @brief x component
-   *
-   * @return T&
-   */
-  T& x() noexcept { return Vector<2, T>::data_[0]; }
+    /**
+     * @brief Construct a new Vector2D object from a Vector object
+     *
+     * also implicitly used for copy initialization. purposely not marked
+     * "explicit" for exactly that usecase. suppress linting for this line.
+     *
+     * @param other
+     */
+    // NOLINTNEXTLINE(runtime/explicit) implicit conversion is intentional
+    Vector2D(const Vector<2, T>& other) : Vector<2, T>(other) {}
 
-  /**
-   * @brief const x component
-   *
-   * @return const T&
-   */
-  const T& x() const noexcept { return Vector<2, T>::data_[0]; }
+    /**
+     * @brief x component
+     *
+     * @return T&
+     */
+    T& x() noexcept { return Vector<2, T>::data_[0]; }
 
-  /**
-   * @brief y component
-   *
-   * @return T&
-   */
-  T& y() noexcept { return Vector<2, T>::data_[1]; }
+    /**
+     * @brief const x component
+     *
+     * @return const T&
+     */
+    const T& x() const noexcept { return Vector<2, T>::data_[0]; }
 
-  /**
-   * @brief const y component
-   *
-   * @return const T&
-   */
-  const T& y() const noexcept { return Vector<2, T>::data_[1]; }
+    /**
+     * @brief y component
+     *
+     * @return T&
+     */
+    T& y() noexcept { return Vector<2, T>::data_[1]; }
 
-  /**
-   * @brief rotates this vector by an angle [rad]
-   *
-   * @tparam T
-   * @param angle
-   * @return std::enable_if<std::is_floating_point<U>::value, void>::type
-   */
-  template <class U = T>
-  typename std::enable_if<std::is_floating_point<U>::value, void>::type rotate(
-      T angle) {
-    const T kX = x();
-    const T kY = y();
-    Vector<2, T>::data_[0] = ((kX * std::cos(angle)) - (kY * std::sin(angle)));
-    Vector<2, T>::data_[1] = ((kX * std::sin(angle)) + (kY * std::cos(angle)));
-  }
+    /**
+     * @brief const y component
+     *
+     * @return const T&
+     */
+    const T& y() const noexcept { return Vector<2, T>::data_[1]; }
 
-  /**
-   * @brief returns a rotated Vector2D
-   *
-   * @see @ref rotate()
-   * @tparam T
-   * @param angle
-   * @return std::enable_if<std::is_floating_point<U>::value, Vector2D<T>>::type
-   */
-  template <class U = T>
-  typename std::enable_if<std::is_floating_point<U>::value, Vector2D<T>>::type
-  rotated(T angle) {
-    Vector2D<T> ret(*this);
-    ret.rotate(angle);
-    return ret;
-  }
-};
+    /**
+     * @brief rotates this vector by an angle [rad]
+     *
+     * @tparam T
+     * @param angle
+     * @return std::enable_if<std::is_floating_point<U>::value, void>::type
+     */
+    template <class U = T>
+    typename std::enable_if<std::is_floating_point<U>::value, void>::type
+    rotate(T angle) {
+      const T kX = x();
+      const T kY = y();
+      Vector<2, T>::data_[0] = ((kX * mu::cos(angle)) - (kY * mu::sin(angle)));
+      Vector<2, T>::data_[1] = ((kX * mu::sin(angle)) + (kY * mu::cos(angle)));
+    }
+
+    /**
+     * @brief returns a rotated Vector2D
+     *
+     * @see @ref rotate()
+     * @tparam T
+     * @param angle
+     * @return std::enable_if<std::is_floating_point<U>::value,
+     * Vector2D<T>>::type
+     */
+    template <class U = T>
+    typename std::enable_if<std::is_floating_point<U>::value, Vector2D<T>>::type
+    rotated(T angle) {
+      Vector2D<T> ret(*this);
+      ret.rotate(angle);
+      return ret;
+    }
+  };
 
 }  // namespace mu
 #endif  // MU_VECTOR2D_H_
