35012bec6d549292deebe5322145168d9a6a03db
m-tosch <m-tosch@users.noreply.github.com>
2020-12-10 23:01:07 +0100

    #15 initial commit

---
+++ include/mu/matrix.h
@@ -0,0 +1,30 @@
+#ifndef MU_MATRIX_H_
+#define MU_MATRIX_H_
+
+#include <array>
+
+namespace mu {
+
+/**
+ * @brief A generic matrix
+ *
+ * @tparam N first matrix dimension
+ * @tparam M second matrix dimension
+ * @tparam T
+ */
+template <std::size_t N, std::size_t M, typename T>
+class Matrix {
+  static_assert(N != 0, "first matrix dimension cannot be zero");
+  static_assert(M != 0, "second matrix dimension cannot be zero");
+
+ public:
+  /**
+   * @brief Construct a new Vector object
+   *
+   */
+  constexpr Matrix() = default;
+
+ protected:
+};
+}  // namespace mu
+#endif  // MU_MATRIX_H_
