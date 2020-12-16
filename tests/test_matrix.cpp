1a1d6e32e11c0670a8da78de3a5f1c85d174901c
m-tosch <m-tosch@users.noreply.github.com>
2020-12-16 12:04:02 +0100

    explicit inst. min(),max()

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -8,6 +8,12 @@
  */
 template class mu::Matrix<2, 2, float>;
 
+/* convenience functions */
+/* these functions take a single Matrix as argument, so they're here. functions
+ * that take e.g a combination of Matrices as argument are elsewhere */
+template float mu::min(const mu::Matrix<2, 2, float> &);
+template float mu::max(const mu::Matrix<2, 2, float> &);
+
 /**
  * Instantiate the Matrix test suite
  */
