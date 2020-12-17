8b2af9f49877125c44f2b17ba3fae48e18ab2f34
m-tosch <m-tosch@users.noreply.github.com>
2020-12-17 18:39:48 +0100

    explicit type on mean() inst.

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -14,14 +14,16 @@ template class mu::Matrix<2, 2, float>;
 template float mu::min(const mu::Matrix<2, 2, float> &);
 template float mu::max(const mu::Matrix<2, 2, float> &);
 template float mu::sum(const mu::Matrix<2, 2, float> &);
-template float mu::mean(const mu::Matrix<2, 2, float> &);
+template float mu::mean<float>(const mu::Matrix<2, 2, float> &);
 
 /**
  * Instantiate the Matrix test suite
  */
-using MatrixTypes =
-    ::testing::Types<mu::Matrix<1, 1, float>, mu::Matrix<2, 1, float>,
-                     mu::Matrix<1, 2, float>, mu::Matrix<2, 2, float>,
-                     mu::Matrix<1, 1, int>, mu::Matrix<2, 1, int>,
-                     mu::Matrix<1, 2, int>, mu::Matrix<2, 2, int>>;
+using MatrixTypes = ::testing::Types<
+    mu::Matrix<1, 1, float>, mu::Matrix<2, 1, float>, mu::Matrix<1, 2, float>,
+    mu::Matrix<2, 2, float>, mu::Matrix<1, 3, float>, mu::Matrix<3, 1, float>,
+    mu::Matrix<2, 3, float>, mu::Matrix<3, 2, float>, mu::Matrix<3, 3, float>,
+    mu::Matrix<1, 1, int>, mu::Matrix<2, 1, int>, mu::Matrix<1, 2, int>,
+    mu::Matrix<2, 2, int>, mu::Matrix<1, 3, int>, mu::Matrix<3, 1, int>,
+    mu::Matrix<2, 3, int>, mu::Matrix<3, 2, int>, mu::Matrix<3, 3, int>>;
 INSTANTIATE_TYPED_TEST_SUITE_P(Matrix, MatrixTypeFixture, MatrixTypes);
