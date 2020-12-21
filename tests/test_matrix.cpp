1c0db12b5f61930958a53442d7645bee94d3a30d
m-tosch <m-tosch@users.noreply.github.com>
2020-12-21 15:27:06 +0100

    small stuff

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -2,9 +2,6 @@
 #include "matrix_type.h"
 #include "mu/matrix.h"
 
-/**
- * Instantiate the Matrix test suite
- */
 using MatrixTypes = ::testing::Types<
     mu::Matrix<1, 1, float>, mu::Matrix<2, 1, float>, mu::Matrix<1, 2, float>,
     mu::Matrix<2, 2, float>, mu::Matrix<1, 3, float>, mu::Matrix<3, 1, float>,
@@ -12,4 +9,5 @@ using MatrixTypes = ::testing::Types<
     mu::Matrix<1, 1, int>, mu::Matrix<2, 1, int>, mu::Matrix<1, 2, int>,
     mu::Matrix<2, 2, int>, mu::Matrix<1, 3, int>, mu::Matrix<3, 1, int>,
     mu::Matrix<2, 3, int>, mu::Matrix<3, 2, int>, mu::Matrix<3, 3, int>>;
+
 INSTANTIATE_TYPED_TEST_SUITE_P(Matrix, MatrixTypeFixture, MatrixTypes);
--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -5,4 +5,5 @@
 using VectorTypes = ::testing::Types<mu::Vector<1, float>, mu::Vector<2, float>,
                                      mu::Vector<3, float>, mu::Vector<1, int>,
                                      mu::Vector<2, int>, mu::Vector<3, int>>;
+
 INSTANTIATE_TYPED_TEST_SUITE_P(Vector, VectorTypeFixture, VectorTypes);
