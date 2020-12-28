3976a91bf59e69e630a54a758db72990972815dc
m-tosch <m-tosch@users.noreply.github.com>
2020-12-28 21:06:52 +0100

    #9 implement gcovr

---
+++ coverage/coverage_gcovr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# project root directory relative to the location of this script
+ROOTDIR="$PWD/.."
+# working directory for intermediate coverage files
+WORKDIR="$ROOTDIR/build/tests/CMakeFiles/mu_tests.dir"
+# coverage directory for a /html report to be generated in
+COVERAGEDIR="$ROOTDIR/coverage"
+
+# 1. cmake build
+# note: same command as in .vscode/tasks.json
+mkdir -p $ROOTDIR/build && cd $ROOTDIR/build && CC=gcc CXX=g++ cmake .. -G "Unix Makefiles" && cmake --build . -- -j8
+# 2. google test main exectuable. only print test failures (generate .gcda files)
+# note: same command as in .vscode/tasks.json
+$ROOTDIR/build/tests/mu_tests --gtest_brief=1
+# 3. generate xml gcovr report. include only the "include" folder for coverage
+mkdir -p $COVERAGEDIR/xml_gcovr
+cd $WORKDIR && gcovr -r $ROOTDIR/.. -f ../../../../include --xml > $COVERAGEDIR/xml_gcovr/index.xml
