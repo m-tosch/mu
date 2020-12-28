0c0205aa8f80e4b382ad051fafea6ac6571ce987
m-tosch <m-tosch@users.noreply.github.com>
2020-12-28 15:41:02 +0100

    #73 Vector-Matrix dot() tests

--- tests/test_vector_combinations.cpp
+++ tests/test_vector_combinations.cpp
@@ -79,6 +79,38 @@ TYPED_TEST(VectorCombinationsFixture, MemberFuncDotVectorVector) {
   EXPECT_EQ(res, comp);
 }
 
+/* test "cheats" by building a Matrix from the second Vector type and size in
+ * the combination. easy way to test the Vector-Matrix dot function. */
+TYPED_TEST(VectorCombinationsFixture, MemberFuncDotVectorMatrix) {
+  /** arrange */
+  static typename TestFixture::T1 dummy1;
+  constexpr auto size = dummy1.size();
+  typename TestFixture::T1 obj1{this->values()};
+  mu::Matrix<size, size, typename TestFixture::BaseTypeFixture2::value_type>
+      obj2;
+  for (auto& row : obj2) {
+    row = this->values2();  // put same values in every row
+  }
+  /** action */
+  mu::Vector<size, typename TestFixture::BaseTypeFixture1::value_type> res;
+  if constexpr (std::is_same_v<typename TestFixture::T1,
+                               typename TestFixture::T2>) {
+    res = obj1.dot(obj2);
+  } else {
+    res = obj1.template dot<typename TestFixture::T1::value_type>(obj2);
+  }
+  /** assert */
+  mu::Vector<size, typename TestFixture::BaseTypeFixture1::value_type> comp;
+  for (std::size_t i = 0; i < size; i++) {
+    typename TestFixture::BaseTypeFixture1::value_type sum{0};
+    for (std::size_t k = 0; k < size; k++) {
+      sum += (obj1[k] * obj2[k][i]);
+    }
+    comp[i] = sum;
+  }
+  EXPECT_TRUE(res == comp);
+}
+
 /************************* convenience functions ***************************/
 
 TYPED_TEST(VectorCombinationsFixture, UtilityFuncDotVectorVector) {
@@ -98,4 +130,34 @@ TYPED_TEST(VectorCombinationsFixture, UtilityFuncDotVectorVector) {
       std::inner_product(obj1.begin(), obj1.end(), obj2.begin(),
                          static_cast<typename TestFixture::T1::value_type>(0));
   EXPECT_EQ(res, comp);
+}
+
+TYPED_TEST(VectorCombinationsFixture, UtilityFuncDotVectorMatrix) {
+  /** arrange */
+  static typename TestFixture::T1 dummy1;
+  constexpr auto size = dummy1.size();
+  typename TestFixture::T1 obj1{this->values()};
+  mu::Matrix<size, size, typename TestFixture::BaseTypeFixture2::value_type>
+      obj2;
+  for (auto& row : obj2) {
+    row = this->values2();  // put same values in every row
+  }
+  /** action */
+  mu::Vector<size, typename TestFixture::BaseTypeFixture1::value_type> res;
+  if constexpr (std::is_same_v<typename TestFixture::T1,
+                               typename TestFixture::T2>) {
+    res = mu::dot(obj1, obj2);
+  } else {
+    res = mu::dot<typename TestFixture::T1::value_type>(obj1, obj2);
+  }
+  /** assert */
+  mu::Vector<size, typename TestFixture::BaseTypeFixture1::value_type> comp;
+  for (std::size_t i = 0; i < size; i++) {
+    typename TestFixture::BaseTypeFixture1::value_type sum{0};
+    for (std::size_t k = 0; k < size; k++) {
+      sum += (obj1[k] * obj2[k][i]);
+    }
+    comp[i] = sum;
+  }
+  EXPECT_TRUE(res == comp);
 }
