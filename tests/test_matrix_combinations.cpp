d6771dcaa1216c5d4833d4cf6de35ba6bffac446
m-tosch <m-tosch@users.noreply.github.com>
2020-12-28 14:31:03 +0100

    #73  Matrix-Matrix dot() tests

--- tests/test_matrix_combinations.cpp
+++ tests/test_matrix_combinations.cpp
@@ -58,3 +58,71 @@ TYPED_TEST(MatrixCombinationsFixture, ConstructorFromArrayOfArrays) {
     EXPECT_THAT(res3[i], ::testing::Pointwise(::testing::FloatEq(), arr[i]));
   }
 }
+
+/* test is only designed for two matrices of exactly the same dimensions */
+TYPED_TEST(MatrixCombinationsFixture, MemberFuncDotMatrixMatrix) {
+  /** arrange */
+  static typename TestFixture::T1 dummy1;
+  static typename TestFixture::T2 dummy2;
+  constexpr auto rows1 = dummy1.size()[0];
+  constexpr auto cols1 = dummy1.size()[1];
+  constexpr auto cols2 = dummy2.size()[1];
+  typename TestFixture::T1 obj1{this->values()};
+  typename TestFixture::T2 obj2{this->values2()};
+  /** action */
+  typename TestFixture::T1 res;
+  if constexpr (std::is_same_v<typename TestFixture::T1,
+                               typename TestFixture::T2>) {
+    res = obj1.dot(obj2);
+  } else {
+    res = obj1.template dot<typename TestFixture::BaseTypeFixture1::value_type>(
+        obj2);
+  }
+  /** assert */
+  typename TestFixture::T1 comp;
+  for (std::size_t i = 0; i < rows1; i++) {
+    for (std::size_t j = 0; j < cols2; j++) {
+      typename TestFixture::BaseTypeFixture1::value_type sum{0};
+      for (std::size_t k = 0; k < cols1; k++) {
+        sum += (obj1[i][k] * obj2[k][j]);
+      }
+      comp[i][j] = sum;
+    }
+  }
+  EXPECT_TRUE(res == comp);
+}
+
+/************************* convenience functions ***************************/
+
+/* test is only designed for two matrices of exactly the same dimensions */
+TYPED_TEST(MatrixCombinationsFixture, UtilityFuncDotMatrixMatrix) {
+  /** arrange */
+  static typename TestFixture::T1 dummy1;
+  static typename TestFixture::T2 dummy2;
+  constexpr auto rows1 = dummy1.size()[0];
+  constexpr auto cols1 = dummy1.size()[1];
+  constexpr auto cols2 = dummy2.size()[1];
+  typename TestFixture::T1 obj1{this->values()};
+  typename TestFixture::T2 obj2{this->values2()};
+  /** action */
+  typename TestFixture::T1 res;
+  if constexpr (std::is_same_v<typename TestFixture::T1,
+                               typename TestFixture::T2>) {
+    res = mu::dot(obj1, obj2);
+  } else {
+    res =
+        mu::dot<typename TestFixture::BaseTypeFixture1::value_type>(obj1, obj2);
+  }
+  /** assert */
+  typename TestFixture::T1 comp;
+  for (std::size_t i = 0; i < rows1; i++) {
+    for (std::size_t j = 0; j < cols2; j++) {
+      typename TestFixture::BaseTypeFixture1::value_type sum{0};
+      for (std::size_t k = 0; k < cols1; k++) {
+        sum += (obj1[i][k] * obj2[k][j]);
+      }
+      comp[i][j] = sum;
+    }
+  }
+  EXPECT_TRUE(res == comp);
+}
