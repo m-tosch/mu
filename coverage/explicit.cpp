db0483a0b815d4ded0e74db9d3279d804db65bd5
m-tosch <m-tosch@users.noreply.github.com>
2021-01-05 17:44:29 +0100

    #107 transpose() inst.

--- coverage/explicit.cpp
+++ coverage/explicit.cpp
@@ -155,7 +155,8 @@ template float mu::sum(const mu::Matrix<2, 2, float> &);
 template float mu::mean<float>(const mu::Matrix<2, 2, float> &);
 template mu::Vector<2, float> mu::diag(const mu::Matrix<2, 2, float> &);
 template int mu::det(const mu::Matrix<2, 2, int> &);
-template mu::Matrix<2, 3, int> mu::transpose(const mu::Matrix<3, 2, int> &);
+template void mu::transpose(mu::Matrix<2, 2, int> &);
+template mu::Matrix<2, 3, int> mu::transposed(const mu::Matrix<3, 2, int> &);
 
 template mu::Matrix<3, 3, int> mu::eye<3>();
 template mu::Matrix<3, 3, int> mu::ones<3, 3>();
